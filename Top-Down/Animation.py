62a513de88d9a680918ebf6ee9c244bba617478f
diguifi <lumia-diguifi@hotmail.com>
2018-07-05 22:19:16 -0300

    Class for animation

---
+++ Top-Down/Animation.py
@@ -0,0 +1,30 @@
+# -*- coding: cp1252 -*-
+"""
+    Copyright (C) 2017 - Instituto Federal de Educacao, Ciencia e Tecnologia do Sul de Minas Gerais, Muzambinho
+    Author: Diego Penha - diego.penha95@gmail.com
+
+
+    This file is part of As Aventuras de Dude no Mundo Top-Down.
+
+    As Aventuras de Dude no Mundo Top-Down is free software:
+    you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    As Aventuras de Dude no Mundo Top-Down
+    is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with As Aventuras de Dude no Mundo Top-Down.
+    If not, see <http://www.gnu.org/licenses/>.
+"""
+import pygame, os
+from random import randint,uniform
+from Configs import *
+
+class animation():
+    print "test"
